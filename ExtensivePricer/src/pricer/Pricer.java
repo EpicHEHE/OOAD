51a9c5849e997f03c70ab26a55dba5b4e35d3558
ChenZheng1 <chenzheng1992@gmail.com>
2014-03-13 21:39:32 +1030

    Modify product and algorithm list

--- ExtensivePricer/src/pricer/Pricer.java
+++ ExtensivePricer/src/pricer/Pricer.java
@@ -3,20 +3,13 @@ package pricer;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
-import javax.swing.JList;
-import java.awt.GridLayout;
 import javax.swing.JLabel;
 import java.awt.Choice;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JTextField;
-import java.awt.FlowLayout;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,14 +18,14 @@ public class Pricer extends JFrame {
 	
 	private JPanel contentPane;
 	
-	private int a;
-	private int b;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField sNaughtPrice;
+	private JTextField strikePrice;
+	private JTextField interestRate;
+	private JTextField term;
+	private JTextField volatility;
+	Choice choiceProduct = new Choice();
+	Choice choiceAlgorithm = new Choice();
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -64,91 +57,99 @@ public class Pricer extends JFrame {
 		contentPane.add(tabbedPane);
 
 		
-		JPanel panel1 = new JPanel();
-		tabbedPane.addTab("Calculator", null, panel1, null);
-		panel1.setLayout(null);
+		JPanel panelCalculator = new JPanel();
+		tabbedPane.addTab("Calculator", null, panelCalculator, null);
+		panelCalculator.setLayout(null);
 
-		JPanel panel2 = new JPanel();
-		tabbedPane.addTab("Extension", null, panel2, null);
+		JPanel panelExtension = new JPanel();
+		tabbedPane.addTab("Extension", null, panelExtension, null);
 		
-		JLabel label = new JLabel("Type");
-		label.setBounds(30, 20, 100, 20);
-		panel1.add(label);
+		JLabel labelType = new JLabel("Type");
+		labelType.setBounds(30, 20, 100, 20);
+		panelCalculator.add(labelType);
 		
-		Choice choice_1 = new Choice();
-		choice_1.setBounds(130, 20, 100, 20);
-		panel1.add(choice_1);
+		choiceProduct.setBounds(130, 20, 150, 20);
+		panelCalculator.add(choiceProduct);
 		ArrayList<String> ProductList = loadProductList();
 		for (int i=0;i<ProductList.size();i++){
-			choice_1.add(ProductList.get(i));	
+			choiceProduct.add(ProductList.get(i));	
 		}
 		
-		JLabel label_1 = new JLabel("Algorithm");
-		label_1.setBounds(30, 50, 100, 20);
-		panel1.add(label_1);
+		JLabel labelAlgorithm = new JLabel("Algorithm");
+		labelAlgorithm.setBounds(30, 50, 100, 20);
+		panelCalculator.add(labelAlgorithm);
+		ArrayList<String> AlgorithmList = loadAlgorithmList();
+		for (int i=0;i<AlgorithmList.size();i++){
+			choiceProduct.add(AlgorithmList.get(i));	
+		}
 		
+		choiceAlgorithm.setBounds(130, 50, 150, 20);
+		panelCalculator.add(choiceAlgorithm);
 		
-		Choice choice_2 = new Choice();
-		choice_2.setBounds(130, 50, 100, 20);
-		panel1.add(choice_2);
-		
-		displayParameter(panel1);
+		displayParameter(panelCalculator);
 	}
 	
 	public ArrayList<String> loadProductList(){
 		ArrayList<String> ProductList = new ArrayList<String>();
-		Collections.addAll(ProductList, "American Option","European Option","Asian Option");
-//		ProductList = ProductAlgorithmManager.getInstance().getProductList();
+//		Collections.addAll(ProductList, "American Option","European Option","Asian Option");
+		ProductList = ProductAlgorithmManager.getInstance().getProductList();
 		
 		return ProductList;
 	}
 	
+	public ArrayList<String> loadAlgorithmList(){
+		ArrayList<String> Algorithmlist = new ArrayList<String>();
+		String product = choiceProduct.getSelectedItem();
+		Algorithmlist = ProductAlgorithmManager.getInstance().getAlgorithmList(product);
+		
+		return Algorithmlist;
+	}
 	
-	public void displayParameter(JPanel panel1){
-		JLabel label_2 = new JLabel("sNaught Price");
-		label_2.setBounds(30, 80, 100, 20);
-		panel1.add(label_2);
+	public void displayParameter(JPanel panelCalculator){
+		JLabel labelSNaught = new JLabel("sNaught Price");
+		labelSNaught.setBounds(30, 80, 100, 20);
+		panelCalculator.add(labelSNaught);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(130, 80, 100, 20);
-		panel1.add(textField_1);
-		textField_1.setColumns(10);
+		sNaughtPrice = new JTextField();
+		sNaughtPrice.setBounds(130, 80, 100, 20);
+		panelCalculator.add(sNaughtPrice);
+		sNaughtPrice.setColumns(10);
 		
-		JLabel label_3 = new JLabel("Strike Price");
-		label_3.setBounds(30, 110, 100, 20);
-		panel1.add(label_3);
+		JLabel labelStrikePrice = new JLabel("Strike Price");
+		labelStrikePrice.setBounds(30, 110, 100, 20);
+		panelCalculator.add(labelStrikePrice);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(130, 110, 100, 20);
-		panel1.add(textField_2);
+		strikePrice = new JTextField();
+		strikePrice.setColumns(10);
+		strikePrice.setBounds(130, 110, 100, 20);
+		panelCalculator.add(strikePrice);
 		
-		JLabel label_4 = new JLabel("Interest Rate");
-		label_4.setBounds(30, 140, 100, 20);
-		panel1.add(label_4);
+		JLabel labelInterest = new JLabel("Interest Rate");
+		labelInterest.setBounds(30, 140, 100, 20);
+		panelCalculator.add(labelInterest);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(130, 140, 100, 20);
-		panel1.add(textField_3);
+		interestRate = new JTextField();
+		interestRate.setColumns(10);
+		interestRate.setBounds(130, 140, 100, 20);
+		panelCalculator.add(interestRate);
 		
-		JLabel label_5 = new JLabel("Term");
-		label_5.setBounds(30, 170, 100, 20);
-		panel1.add(label_5);
+		JLabel labelTerm = new JLabel("Term");
+		labelTerm.setBounds(30, 170, 100, 20);
+		panelCalculator.add(labelTerm);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(130, 170, 100, 20);
-		panel1.add(textField_4);
+		term = new JTextField();
+		term.setColumns(10);
+		term.setBounds(130, 170, 100, 20);
+		panelCalculator.add(term);
 		
-		JLabel label_6 = new JLabel("Volatility");
-		label_6.setBounds(30, 200, 100, 20);
-		panel1.add(label_6);
+		JLabel labelVolatility = new JLabel("Volatility");
+		labelVolatility.setBounds(30, 200, 100, 20);
+		panelCalculator.add(labelVolatility);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(130, 200, 100, 20);
-		panel1.add(textField_5);
+		volatility = new JTextField();
+		volatility.setColumns(10);
+		volatility.setBounds(130, 200, 100, 20);
+		panelCalculator.add(volatility);
 
 
 	}
