b64cabdfaf90dd46962bfd7e1fcecafbd424ff1a
ChenZheng1 <chenzheng1992@gmail.com>
2014-03-14 12:57:59 +1030

    almost complete calculate panel

--- ExtensivePricer/src/pricer/Pricer.java
+++ ExtensivePricer/src/pricer/Pricer.java
@@ -29,16 +29,14 @@ public class Pricer extends JFrame {
 	
 	private JPanel contentPane;
 	
-/*	private JTextField sNaughtPrice;
-	private JTextField strikePrice;
-	private JTextField interestRate;
-	private JTextField term;
-	private JTextField volatility;
-*/	Choice choiceProduct = new Choice();
+	Choice choiceProduct = new Choice();
 	Choice choiceAlgorithm = new Choice();
 	JPanel panelCalculator = new JPanel();
+	JPanel panelExtension = new JPanel();
     JLabel labelPut = new JLabel();
 	JLabel labelCall = new JLabel();
+	TreeMap<String, JTextField> textFieldMap = new TreeMap<String, JTextField>();
+	TreeMap<String, Double> parameterInputMap = new TreeMap<String, Double>();
 
 
 	public static void main(String[] args) {
@@ -77,7 +75,6 @@ public class Pricer extends JFrame {
 		tabbedPane.addTab("Calculator", null, panelCalculator, null);
 		panelCalculator.setLayout(null);
 
-		JPanel panelExtension = new JPanel();
 		tabbedPane.addTab("Extension", null, panelExtension, null);
 		
 		JButton addJARButton = new JButton("add JAR");
@@ -105,7 +102,7 @@ public class Pricer extends JFrame {
 		choiceProduct.setBounds(130, 20, 150, 20);
 		panelCalculator.add(choiceProduct);
 		ArrayList<String> ProductList = loadProductList();
-		for (int i=0;i<ProductList.size();i++){
+		for (int i = 0; i < ProductList.size(); i++){
 			choiceProduct.add(ProductList.get(i));	
 		}
 		
@@ -113,7 +110,7 @@ public class Pricer extends JFrame {
 		labelAlgorithm.setBounds(30, 50, 100, 20);
 		panelCalculator.add(labelAlgorithm);
 		ArrayList<String> AlgorithmList = loadAlgorithmList();
-		for (int i=0;i<AlgorithmList.size();i++){
+		for (int i = 0; i < AlgorithmList.size(); i++){
 			choiceProduct.add(AlgorithmList.get(i));	
 		}
 		
@@ -122,9 +119,8 @@ public class Pricer extends JFrame {
 				
 		ArrayList<String> parameterList = new ArrayList<String>();
 		parameterList = loadParameter(choiceProduct.getSelectedItem(), choiceAlgorithm.getSelectedItem());
-		TreeMap<String, Double> parameterInputMap = new TreeMap<String, Double>();
 		
-		for (int j=0;j<parameterList.size();j++){
+		for (int j = 0; j < parameterList.size(); j++){
 			JLabel label = new JLabel(parameterList.get(j));
 			label.setBounds(30, 80+30*j, 100, 20);
 			panelCalculator.add(label);
@@ -133,9 +129,8 @@ public class Pricer extends JFrame {
 			textFieldName.setBounds(130, 80+30*j, 150, 20);
 			panelCalculator.add(textFieldName);
 			textFieldName.setColumns(10);
-			textFieldName.getInputContext();
 			
-//			parameterInputMap.put(parameterList.get(j), textFieldName.getInputContext());
+			textFieldMap.put(parameterList.get(j), textFieldName);
 
 		}
 		
@@ -163,17 +158,28 @@ public class Pricer extends JFrame {
 				labelCall.setText(String.valueOf(priceArray[1]));
 			}
 		});
+		
+		JButton volatilityButton = new JButton("Show Volatility Smile");
+		volatilityButton.setBounds(400, 80+30*parameterList.size(), 200, 25);
+		panelCalculator.add(volatilityButton);
 
 	}
 	
 	public double[] calculatePrice(){
 		double [] priceArray = new double[2];
-		priceArray[0] = 1.0;
-		priceArray[1] = 2.0;
-//		String productName = choiceProduct.getSelectedItem();
-//		String algorithmName = choiceAlgorithm.getSelectedItem();
+//		priceArray[0] = 1.0;
+//		priceArray[1] = 2.0;
+
+		String productName = choiceProduct.getSelectedItem();
+		String algorithmName = choiceAlgorithm.getSelectedItem();
+		for (String parameterName: textFieldMap.keySet()){
+			parameterInputMap.put(parameterName, Double.parseDouble(textFieldMap.get(parameterName).getText()));
+		}
 		
-//		priceArray = ProductAlgorithmManager.getInstance().getAlgorithmSelected(productName, algorithmName).calculate();
+		Algorithm algorithm = ProductAlgorithmManager.getInstance().getAlgorithmSelected(productName, algorithmName);
+		algorithm.setParameter(parameterInputMap);
+		priceArray = algorithm.calculate();
+		
 		return priceArray;
 	}
 	
@@ -197,51 +203,5 @@ public class Pricer extends JFrame {
 //		parameterList =	ProductAlgorithmManager.getInstance().getParameterList(productName, algorithmName);
 		Collections.addAll(parameterList, "sNaught Price","Strike Price","Interest Rate", "Term", "Volatility");
 		return parameterList;
-/*		JLabel labelSNaught = new JLabel("sNaught Price");
-		labelSNaught.setBounds(30, 80, 100, 20);
-		panelCalculator.add(labelSNaught);
-		
-		sNaughtPrice = new JTextField();
-		sNaughtPrice.setBounds(130, 80, 150, 20);
-		panelCalculator.add(sNaughtPrice);
-		sNaughtPrice.setColumns(10);
-		
-		JLabel labelStrikePrice = new JLabel("Strike Price");
-		labelStrikePrice.setBounds(30, 110, 100, 20);
-		panelCalculator.add(labelStrikePrice);
-		
-		strikePrice = new JTextField();
-		strikePrice.setColumns(10);
-		strikePrice.setBounds(130, 110, 150, 20);
-		panelCalculator.add(strikePrice);
-		
-		JLabel labelInterest = new JLabel("Interest Rate");
-		labelInterest.setBounds(30, 140, 100, 20);
-		panelCalculator.add(labelInterest);
-		
-		interestRate = new JTextField();
-		interestRate.setColumns(10);
-		interestRate.setBounds(130, 140, 150, 20);
-		panelCalculator.add(interestRate);
-		
-		JLabel labelTerm = new JLabel("Term");
-		labelTerm.setBounds(30, 170, 100, 20);
-		panelCalculator.add(labelTerm);
-		
-		term = new JTextField();
-		term.setColumns(10);
-		term.setBounds(130, 170, 150, 20);
-		panelCalculator.add(term);
-		
-		JLabel labelVolatility = new JLabel("Volatility");
-		labelVolatility.setBounds(30, 200, 100, 20);
-		panelCalculator.add(labelVolatility);
-		
-		volatility = new JTextField();
-		volatility.setColumns(10);
-		volatility.setBounds(130, 200, 150, 20);
-		panelCalculator.add(volatility);
-*/
-
 	}
 }
